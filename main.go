83624eb3ee638ed83a97b88376ea471a72394ff9
Anny Gil <anny.gil@diconium.com>
2024-08-15 15:58:02 +0200

    feat(server): add a readiness endpoint to the Chirpy server

--- main.go
+++ main.go
@@ -4,16 +4,25 @@ import (
 	"net/http"
 )
 
+func customHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plain; charset=utf-8") // normal header
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("OK"))
+
+}
 func main() {
 	//Create a new http.ServeMux
 	mux := http.NewServeMux()
 	//Create a new http.Server struct and use the new "ServeMux" as the server's handler
-	srv := http.Server{
+	srv := &http.Server{
 		Addr:    ":8080",
 		Handler: mux,
 	}
-	mux.Handle("/", http.FileServer(http.Dir(".")))
+	//mux.HandleFunc("/users", customHandler)
+	mux.HandleFunc("/healthz", customHandler)
+	mux.Handle("/app/*", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
 	mux.Handle("/assets", http.FileServer(http.Dir("./assets/")))
+
 	//Use the server's ListenAndServe method to start the server
 	srv.ListenAndServe()
 
