5577d224f85e031d6ffd5757da8e693312438e23
Anny Gil <anny.gil@diconium.com>
2024-08-15 15:18:42 +0200

    feat(server): build and run a server that binds to localhost:8080 and always responds with a 404 Not Found response

--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+)
 
 func main() {
-	fmt.Println("Hello, world.")
+	//Create a new http.ServeMux
+	mux := http.NewServeMux()
+	//Create a new http.Server struct and use the new "ServeMux" as the server's handler
+	srv := http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+	//Use the server's ListenAndServe method to start the server
+	srv.ListenAndServe()
+
 }
